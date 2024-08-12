218451373275a8e0d6bdf0ced7ac2bfbe3a36186
timmchi <tnmagg@gmail.com>
2024-08-12 13:22:06 +0300

    change to location fetcher, no more apiType state

--- frontend/src/components/Map/LocationFetcher.jsx
+++ frontend/src/components/Map/LocationFetcher.jsx
@@ -5,16 +5,15 @@ import StreetView from "./StreetView";
 import useRandomLocation from "../../hooks/useRandomLocation";
 
 const LocationFetcher = ({ calculateScore, roomMapType }) => {
-  // geolist backend api is suited more to the entire world map, as it is random points across the world, while geonames can be limited to a specific country, hence why it is used with the 'country' roomMapType
-  const [apiType, setApiType] = useState(
-    roomMapType === "world" ? "geolist" : "geonames"
-  );
   const [location, setLocation] = useState({ lat: 45, lng: 45 });
 
   // uncomment if checking whether the map loc is same as pano, otherwise no need
   //   const map = useMap();
 
-  const { isLoading, data, refetch } = useRandomLocation(apiType);
+  // geolist backend api is suited more to the entire world map, as it is random points across the world, while geonames can be limited to a specific country, hence why it is used with the 'country' roomMapType
+  const { isLoading, data, refetch } = useRandomLocation(
+    roomMapType === "world" ? "geolist" : "geonames"
+  );
 
   if (isLoading) return <div>Loading...</div>;
 
@@ -34,12 +33,6 @@ const LocationFetcher = ({ calculateScore, roomMapType }) => {
         lat: {data?.lat}, lng: {data?.lng}
       </p>
       <div className="flex">
-        <Button variant="outlined" onClick={() => setApiType("geolist")}>
-          set to geolist
-        </Button>
-        <Button variant="outlined" onClick={() => setApiType("geonames")}>
-          set to geonames
-        </Button>
         <Button variant="outlined" onClick={fetchLocation}>
           Fetch location
         </Button>
