98af0e3276afe14ea06b7a5dd3b019c45472b56d
timmchi <tnmagg@gmail.com>
2024-08-29 13:22:00 +0300

    log in outline

--- frontend/src/components/Authentication/LogIn.jsx
+++ frontend/src/components/Authentication/LogIn.jsx
@@ -1,5 +1,14 @@
+import LogInForm from "./LogInForm";
+
 const LogIn = () => {
-  return <div>Log in form goes here</div>;
+  return (
+    <div className="flex h-screen">
+      <div className="flex md:basis-1/2 justify-center bg-indigo-300 text-white items-center">
+        <LogInForm />
+      </div>
+      <div className="flex md:basis-1/2 bg-cover bg-center"></div>
+    </div>
+  );
 };
 
 export default LogIn;
