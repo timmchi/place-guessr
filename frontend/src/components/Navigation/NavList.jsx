f26413290243ee5588b239cf6a9826d5bded2da8
timmchi <tnmagg@gmail.com>
2024-10-04 15:08:12 +0300

    navbar style change

--- frontend/src/components/Navigation/NavList.jsx
+++ frontend/src/components/Navigation/NavList.jsx
@@ -6,43 +6,28 @@ const NavList = ({ handleLogout }) => {
   const user = useSelector((state) => state.user);
 
   return (
-    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
-      <Typography
-        as="li"
-        variant="paragraph"
-        color="blue-gray"
-        className="p-1 font-medium"
-      >
+    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6 text-amber-300">
+      <Typography as="li" variant="h5" className="p-1 font-large">
         <Link
           to="/about"
-          className="flex items-center hover:text-blue-500 transition-colors"
+          className="flex items-center hover:text-amber-600 transition-colors"
         >
           About
         </Link>
       </Typography>
       {user ? (
         <>
-          <Typography
-            as="li"
-            variant="paragraph"
-            color="blue-gray"
-            className="p-1 font-medium"
-          >
+          <Typography as="li" variant="h5" className="p-1 font-large">
             <Link
-              className="flex items-center hover:text-blue-500 transition-colors"
+              className="flex items-center hover:text-amber-600 transition-colors"
               onClick={handleLogout}
             >
               Log Out
             </Link>
           </Typography>
-          <Typography
-            as="li"
-            variant="paragraph"
-            color="blue-gray"
-            className="p-1 font-medium"
-          >
+          <Typography as="li" variant="h5" className="p-1 font-large">
             <Link
-              className="flex items-center hover:text-blue-500 transition-colors"
+              className="flex items-center hover:text-amber-600 transition-colors"
               to={`/users/${user.id}`}
             >
               Profile
@@ -51,28 +36,18 @@ const NavList = ({ handleLogout }) => {
         </>
       ) : (
         <>
-          <Typography
-            as="li"
-            variant="paragraph"
-            color="blue-gray"
-            className="p-1 font-medium"
-          >
+          <Typography as="li" variant="h5" className="p-1 font-large">
             <Link
               to="/login"
-              className="flex items-center hover:text-blue-500 transition-colors"
+              className="flex items-center hover:text-amber-600 transition-colors"
             >
               Log In
             </Link>
           </Typography>
-          <Typography
-            as="li"
-            variant="paragraph"
-            color="blue-gray"
-            className="p-1 font-medium"
-          >
+          <Typography as="li" variant="h5" className="p-1 font-large">
             <Link
               to="/register"
-              className="flex items-center hover:text-blue-500 transition-colors"
+              className="flex items-center hover:text-amber-600 transition-colors"
             >
               Sign Up
             </Link>
