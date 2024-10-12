064ff8355ec65d79fcb2aeb3886ee1202aeb7768
timmchi <tnmagg@gmail.com>
2024-10-12 11:41:34 +0300

    ability to end single game added

--- frontend/src/components/SingleGame/SingleGameEndScreen.jsx
+++ frontend/src/components/SingleGame/SingleGameEndScreen.jsx
@@ -1,6 +1,6 @@
 import { Button, Avatar, Typography } from "@material-tailwind/react";
 
-const SingleGameEndScreen = ({ room, totalScore, handleGameEnd }) => {
+const SingleGameEndScreen = ({ room, totalScore, handleGameExit, round }) => {
   return (
     <div className="h-screen bg-indigo-300 flex flex-col justify-center items-center">
       <Avatar
@@ -11,15 +11,15 @@ const SingleGameEndScreen = ({ room, totalScore, handleGameEnd }) => {
         src={room.flag}
       />
       <Typography variant="h1">
-        {room.region !== "random" ? room.title : "the world"}
+        {room.region !== "random" ? room.title : "The World"}
       </Typography>
       <div className="text-center pt-8">
         <Typography variant="h2">Game is complete</Typography>
         <Typography variant="h3">
-          You got {Math.floor(totalScore)} out of 25000 points
+          You got {Math.floor(totalScore)} out of {5000 * round} points
         </Typography>
         <Button
-          onClick={handleGameEnd}
+          onClick={handleGameExit}
           className="mt-4 w-full p-4 text-lg font-bold bg-amber-200 hover:bg-amber-400 text-indigo-500"
         >
           Back to main page
